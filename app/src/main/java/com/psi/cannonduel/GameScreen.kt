7e187e4481dfc2367f187c700a5ffed956026ac8
Pirito10 <aaronriveiro@gmail.com>
2024-12-15 16:15:48 +0100

    creado fichero para la pantalla de juego

---
+++ app/src/main/java/com/psi/cannonduel/GameScreen.kt
@@ -0,0 +1,13 @@
+package com.psi.cannonduel
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+// Función para la pantalla de juego
+@Composable
+fun GameScreen() {
+    // Contenedor que ocupa toda la pantalla
+    Box(modifier = Modifier.fillMaxSize())
+}
